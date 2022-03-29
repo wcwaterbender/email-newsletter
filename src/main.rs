a6fd47d2b31eaf4f848b1ff7f87addc5990c3529
Sam Cornish <samcornish96@gmail.com>
2022-03-28 22:00:52 -0700

    add dockerfile to support building and deploying app in containers, updated configuration settings to be hierarchial

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use email_newsletter::configuration::get_configuration;
 use email_newsletter::startup::run;
 use email_newsletter::telemetry::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[actix_web::main] // or #[tokio::main]
@@ -15,11 +15,14 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read Config");
 
     //create our pool of connections to the database we are using
-    let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
 
     //ready set go
